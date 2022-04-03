a86abff1b56630716fc2a6804cf9a5fef9b1e1da
Joshhortt <joshhortt@yahoo.com>
2022-04-03 13:07:42 +0100

    style hamburger menu + add script'.

---
+++ script.js
@@ -0,0 +1,11 @@
+var menu = document.getElementById("bar");
+var item = document.getElementById("item");
+
+item.style.right = '-300px';
+
+if (item.style.right == '-300px')  {
+    item.style.right == '0';
+}
+else {
+    item.style.right == '-300px';
+}
